1be562477c71e4db3b93ac506656055f53328483
pritam-dey-19 <dey.pritam19@gmail.com>
2023-04-23 20:53:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("div1").innerHTML =
+  " <b>width :</b> " +
+  window.innerWidth +
+  "px<br>" +
+  "<b>height :</b> " +
+  window.innerHeight +
+  "px";
+document.getElementById("div2").innerHTML =
+  "<b>URL :</b> " + window.location.href;
